7315caafc5920063df9d03398a46eb73e034103b
Nico-CP00 <ncespedes@utem.cl>
2025-09-02 21:47:14 -0400

    ejercicio 3

---
+++ Ejercicio 3.py
@@ -0,0 +1,23 @@
+from Estudiantes import lis_est
+import pandas as pd
+
+'''
+3. ¿Cúal es la nota más frecuente considerando todas las notas de todos los estudiantes?
+'''
+
+todas_notas = []
+
+# Como el diccionario está compuesto por más diccionarios, debemos usar ciclos for para recorrerlos y separar así todas las notas
+for lista in lis_est["notas"]:
+    for n in lista:
+        todas_notas.append(n)
+
+# Usamos la librería Pandas para obtener la moda o las modas
+moda = pd.Series(todas_notas).mode()
+
+# Para ver cuantas veces se repite la nota más frecuente
+notas_series = pd.Series(todas_notas)
+notas_frec = notas_series.value_counts()
+frecu = max(notas_frec)
+
+print("La(s) nota(s) más frecuentes es(son): ", moda.tolist(), "y se repiten ", frecu, "veces.")
